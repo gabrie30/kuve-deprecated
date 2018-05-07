9579d8ea718ebce8cac5666dc74e1cd094442d2c
Jay Gabriels <jgabriels30@gmail.com>
2018-05-07 10:53:24 -0700

    add a configurable shell for exec command

--- exec.cr
+++ exec.cr
@@ -3,14 +3,19 @@ class Exec
     @namespace = namespace
   end
 
+  def default_shell
+    # If no context flag is supplied just use default
+    JSON.parse(File.open("/usr/local/bin/kuve_conf.json"))["shell"]["exec-default"]
+  end
+
   def exec_into_pod
     pod_name = get_pod_name.chomp
     env = `kubectl config current-context`
     puts "###### Running the following command in  #{env.chomp}  ######"
     puts ""
-    puts "$ kubectl exec -it #{pod_name} -n #{@namespace} -- /bin/bash"
+    puts "$ kubectl exec -it #{pod_name} -n #{@namespace} -- #{default_shell}"
     puts ""
-    system("kubectl exec -it #{pod_name} -n #{@namespace} -- /bin/bash")
+    system("kubectl exec -it #{pod_name} -n #{@namespace} -- #{default_shell}")
   end
 
   def get_pod_name
