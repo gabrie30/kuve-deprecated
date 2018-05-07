e6d7d9c1f827212f64c819b530f90fa87de085df
Jay Gabriels <jgabriels30@gmail.com>
2018-05-07 11:21:59 -0700

    allow users to pass in optional shell to exec

--- exec.cr
+++ exec.cr
@@ -1,11 +1,17 @@
 class Exec
-  def initialize(namespace : String)
+  def initialize(namespace : String, chosen_shell : String)
     @namespace = namespace
+
+    if chosen_shell == "false"
+      @shell = default_shell
+    else
+      @shell = chosen_shell
+    end
   end
 
   def default_shell
     # If no context flag is supplied just use default
-    JSON.parse(File.open("/usr/local/bin/kuve_conf.json"))["shell"]["exec-default"]
+    JSON.parse(File.open("/usr/local/bin/kuve_conf.json"))["shell"]["exec-default"].to_s
   end
 
   def exec_into_pod
@@ -13,9 +19,9 @@ class Exec
     env = `kubectl config current-context`
     puts "###### Running the following command in  #{env.chomp}  ######"
     puts ""
-    puts "$ kubectl exec -it #{pod_name} -n #{@namespace} -- #{default_shell}"
+    puts "$ kubectl exec -it #{pod_name} -n #{@namespace} -- #{@shell}"
     puts ""
-    system("kubectl exec -it #{pod_name} -n #{@namespace} -- #{default_shell}")
+    system("kubectl exec -it #{pod_name} -n #{@namespace} -- #{@shell}")
   end
 
   def get_pod_name
